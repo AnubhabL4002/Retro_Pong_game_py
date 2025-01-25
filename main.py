368610e0952c22fca5099ad9db2d2e01429e8b76
Anubhab Lahiri <158812807+AnubhabL4002@users.noreply.github.com>
2025-01-26 01:35:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
+scoreboard = Scoreboard()
 
 screen.title("Pong: The Arcade Game")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
-paddle = Paddle()
+l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350,0))
+ball = Ball((0,0))
 
 screen.listen()
-screen.onkey(paddle.up, "w")
-screen.onkey(paddle.down, "s")
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
+screen.onkeypress(r_paddle.up, "p")
+screen.onkeypress(r_paddle.down, ";")
 
 game_is_on= True
 while game_is_on:
     screen.update()
-    # time.sleep(0.1)
+    ball.move_ball()
+    time.sleep(ball.move_speed)
 
+    # Detect collision with the upper and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Using Pythagoras Theorem
+    # if (ball.xcor() == 330 and ball.distance(r_paddle) < 63) or (ball.xcor() == -330 and ball.distance(l_paddle) < 63):
+    #     ball.bounce_x()
+
+
+    if (ball.distance(r_paddle) < 63 and ball.xcor() > 320 and ball.x_move > 0
+            or ball.distance(l_paddle) <= 63 and ball.xcor() < -320
+            and ball.x_move < 0):
+        ball.bounce_x()
+
+
+    # Detect collision with right paddle
+    # if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+    #     ball.bounce_x_r_paddle()
+    #
+    # # Detect collision with left paddle
+    # if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    #     ball.bounce_x_l_paddle()
+
+    # Detect collision with right wall
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    # Detect collision with left wall
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_position()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,17 +1,18 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
-class Paddle:
-    def __init__(self):
-        self.p_body = Turtle("square")
-        self.p_body.color("blue")
-        self.p_body.shapesize(stretch_wid=5, stretch_len=1)
-        self.p_body.penup()
-        self.p_body.goto(x=350, y=0)
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("dark violet")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def up(self):
-        new_y = self.p_body.ycor() + MOVE_DISTANCE
-        self.p_body.goto(self.p_body.xcor(),new_y)
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(),new_y)
     def down(self):
-        new_y = self.p_body.ycor() - MOVE_DISTANCE
-        self.p_body.goto(self.p_body.xcor(),new_y)
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(),new_y)
